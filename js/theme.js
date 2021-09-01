825ef73667600dc257af35a495b10f825e6a31a7
Raphoulfifou <81476169+Raphoulfifou@users.noreply.github.com>
2021-09-01 19:15:36 +0200

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -1,52 +1,24 @@
 /* Theme functions */
 
-let syncThemeWithOS = Boolean(document.getElementById("syncWithOS"));
-const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-
-function getUserOSMode()
+function loadUtils()
 {
-    window.matchMedia("(prefers-color-scheme)")
+    applyMode();
+    checkPressedSliders();
 }
 
-function setUserModeFromOS()
+function changeMode()
 {
-    var selectedTheme = document.getElementById("Theme");
-
-    if (syncThemeWithOS == true)
-    {
-        if (darkThemeMq.matches)
-        {
-            setMode("dark");
-        }
-        if (!darkThemeMq.matches)
-        {
-            setMode("light");
-        }
-        
-    }
-    if (syncThemeWithOS == false)
-    {
-        if (selectedTheme == "Dark Mode")
-        {
-            changeMode();
-        }
-        if (selectedTheme == false)
-        {
-            changeMode();
-        }
-    }
-}
-
-function changeMode() {
-	var currentUsedCSSFile = document.getElementById("theme");
+    var currentUsedCSSFile = document.getElementById("theme");
 	var currentUsedModeIcon = document.getElementById("mode_icon");
 
-	if (currentUsedModeIcon.getAttribute("class") == "fas fa-sun" ) {
+	if (currentUsedModeIcon.getAttribute("class") == "fas fa-sun")
+    {
 		currentUsedCSSFile.setAttribute("href", "css/light.css");
 		currentUsedModeIcon.setAttribute("class", "fas fa-moon");
 		localStorage.setItem("mode", "light");
 	}
-	else if (currentUsedModeIcon.getAttribute("class") == "fas fa-moon") {
+	else if (currentUsedModeIcon.getAttribute("class") == "fas fa-moon")
+    {
 		currentUsedCSSFile.setAttribute("href", "css/dark.css");
 		currentUsedModeIcon.setAttribute("class", "fas fa-sun");
 		localStorage.setItem("mode", "dark");
@@ -57,12 +29,85 @@ function applyMode() {
 	var mode = localStorage.getItem("mode");
 	var currentUsedModeIcon = document.getElementById("mode_icon");
 
-	if (mode == "dark") {
-		document.getElementById("theme").setAttribute("href", "css/dark.css");
-		currentUsedModeIcon.setAttribute("class", "fas fa-sun");
-	}
-	else if (mode == "light") {
-		document.getElementById("theme").setAttribute("href", "css/light.css");
-		currentUsedModeIcon.setAttribute("class", "fas fa-moon");
+    if (localStorage.getItem("SyncWOS") == true)
+    {
+        changeModeOS();
     }
+    else if (localStorage.getItem("SyncWOS") ==  false)
+    {
+        if (mode == "dark")
+        {
+            document.getElementById("theme").setAttribute("href", "css/dark.css");
+            currentUsedModeIcon.setAttribute("class", "fas fa-sun");
+        }
+        else if (mode == "light")
+        {
+            document.getElementById("theme").setAttribute("href", "css/light.css");
+            currentUsedModeIcon.setAttribute("class", "fas fa-moon");
+        }
+    }	
+}
+
+function checkPressedSliders()
+{
+    if (localStorage.getItem("SyncWOS") == true)
+    {
+        document.getElementById("OSSlider").click.call;
+    }
+}
+
+function setOSItem(doClick)
+{
+    if (doClick == true)
+    {
+        if (localStorage.getItem("SyncWOS") == null)
+        {
+            localStorage.setItem("SyncWOS", true);
+        }
+        else
+        {
+            if (localStorage.getItem("SyncWOS") == true)
+            {
+                localStorage.setItem("SyncWOS", false);
+            }
+            else if (localStorage.getItem("SyncWOS") == false)
+            {
+                localStorage.setItem("SyncWOS", true);
+            }
+        }
+    }
+}
+
+function changeModeOS()
+{
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    var currentUsedCSSFile = document.getElementById("theme");
+    var currentUsedModeIcon = document.getElementById("mode_icon");
+
+    darkThemeMq.addListener(e => {
+        if (e.matches)
+        {
+            if (currentUsedModeIcon.getAttribute("class") == "fas fa-moon")
+            {
+                currentUsedCSSFile.setAttribute("href", "css/dark.css");
+                currentUsedModeIcon.setAttribute("class", "fas fa-sun");
+                localStorage.setItem("mode", "dark");
+            }
+            currentUsedCSSFile.setAttribute("href", "css/light.css");
+            currentUsedModeIcon.setAttribute("class", "fas fa-moon");
+            localStorage.setItem("mode", "light");
+        }
+        else
+        {
+            if (currentUsedModeIcon.getAttribute("class") == "fas fa-moon")
+            {
+                currentUsedCSSFile.setAttribute("href", "css/light.css");
+                currentUsedModeIcon.setAttribute("class", "fas fa-moon");
+                localStorage.setItem("mode", "light");
+            }
+            currentUsedCSSFile.setAttribute("href", "css/dark.css");
+            currentUsedModeIcon.setAttribute("class", "fas fa-sun");
+            localStorage.setItem("mode", "dark");
+        }
+    });
 }
