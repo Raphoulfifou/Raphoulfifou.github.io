d9c31fb2b2a5f2db6856912ee7a1ea34dfc8cdfe
Raphoulfifou <81476169+Raphoulfifou@users.noreply.github.com>
2021-09-15 19:58:07 +0200

    Add files via upload

--- js/lang.js
+++ js/lang.js
@@ -1,18 +1,32 @@
 /* Functions for language */
-let availableLanguages = {en, fr};
 
-function getUserLanguage()
-{}
+var dict = {
+    en: {
+        "SyncThemeWOS": "Synchronise language with system:",
+        "SyncLangWOS": "Synchronise language with system:"
+    },
 
-function setActiveButton(button)
-{
-    var currentlyActive = document.getElementsByClassName("active_button");
-
-    let en = document.getElementById("en");
-    let fr = document.getElementById("fr");
-    
-    if (button == fr && button != currentlyActive)
-    {
-        button.setAttribute("class", "active_button")
+    fr: {
+        "SyncThemeWOS": "Synchronise language with system:",
+        "SyncLangWOS": "Synchroniser la langue avec le système:"
     }
 }
+
+var lang = "en";
+//var tmpl = "<div>{{Goodbye}}, {{castle}}</div>";
+
+function translate(dict, lang, word) {
+    return dict[lang][word];
+}
+
+function applyTemplate(tmpl, lang) {
+    var regex = /\{\{([a-zA-Z])\w+\}\}/g
+    return tmpl.replace(regex, function (word) {
+        return translate(dict, lang, word.replace(/[\{\}]/g, ""));
+    });
+}
+
+var tmpl = document.querySelector(".template").textContent;
+var translation  = document.querySelector("#translation");
+var html = applyTemplate(tmpl, lang);
+translation.insertAdjacentHTML("afterbegin", html);
